05b8788484f4a20d39b28d85c2ebf9e9cc0c17e6
Matthew Berryman <matthew@acrossthecloud.net>
2018-01-03 14:07:13 +1100

    Try 10 seconds between requests

--- R/gcite_user_info.R
+++ R/gcite_user_info.R
@@ -4,11 +4,11 @@
 #' @param pagesize Size of pages, max 100, passed to \code{\link{gcite_url}}
 #' @param verbose Print diagnostic messages
 #' @param secure use https vs. http
-#' 
+#'
 #' @param ... Not used
 #'
-#' @return A list of citations, citation indices, and a 
-#' \code{data.frame} of authors, journal, and citations, and a 
+#' @return A list of citations, citation indices, and a
+#' \code{data.frame} of authors, journal, and citations, and a
 #' \code{data.frame} of the links to all paper URLs and the character
 #' string of the user name.
 #' @export
@@ -18,55 +18,56 @@
 #' df = gcite_user_info(user = "T9eqZgMAAAAJ")
 #' }
 gcite_user_info = function(
-  user, pagesize = 100, 
-  verbose = TRUE, 
+  user, pagesize = 100,
+  verbose = TRUE,
   secure = TRUE,
   ...) {
-  url = paste0("http", ifelse(secure, "s", ""), 
+  url = paste0("http", ifelse(secure, "s", ""),
                "://scholar.google.com/citations?user=", user)
-  
+
   #############################################
   # Getting initial URL
-  #############################################  
+  #############################################
   url = gcite_url(url, pagesize = pagesize, cstart = 0)
   res = httr::GET(url = url)
   stop_for_status(res)
   doc = httr::content(res)
-  
+
   if (verbose) {
     message("Getting Citation Index table")
   }
   cite_ind = gcite_citation_index(doc)
-  
+
   if (verbose) {
     message("Getting Overall citations over time")
-  }  
+  }
   # overall_cite = gcite_main_graph(doc)
   overall_cite = gcite_cite_over_time(doc)
-  
+
   if (verbose) {
     message("Getting first set of papers")
-  }  
+  }
   papers = gcite_papers(doc)
-  
+
   if (verbose) {
     message("Getting rest of papers")
-  }  
+  }
   cstart = pagesize + 1
-  
+
   #######################
   # looping over papers
   all_papers = papers
   while (!is.null(papers)) {
     url = gcite_url(url, pagesize = pagesize, cstart = cstart)
     papers = gcite_papers(url)
+    Sys.sleep(10)
     all_papers = rbind(all_papers, papers)
     cstart = pagesize + cstart
   }
-  
+
   if (verbose) {
     message("Reading citation pages")
-  }  
+  }
   urls = all_papers$title_link
   paper_info = pbapply::pblapply(urls, gcite_citation_page)
   paper_df = data.table::rbindlist(paper_info, fill = TRUE)
@@ -77,7 +78,7 @@ gcite_user_info = function(
   })
   cn = c(cn[is.na(num_cn)], sort(num_cn[ !is.na(num_cn)]))
   paper_df = paper_df[, cn]
-  
+
   # paper_df$title =
   L = list(citation_indices = cite_ind,
            overall_citations = overall_cite,
@@ -87,4 +88,3 @@ gcite_user_info = function(
   )
   return(L)
 }
-
